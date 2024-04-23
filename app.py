30a061b7275137bee9990f59802d80ca904a06be
Blane187 <161300198+Blane187@users.noreply.github.com>
2024-04-23 15:16:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,34 +14,34 @@ ZERO_GPU_PATCH_TORCH_DEVICE = 1
 # Configura o logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-logging.info("Carregando o modelo pré-treinado.")
+logging.info("Loading the pre-trained model.")
 model = MusicGen.get_pretrained('nateraw/musicgen-songstarter-v0.2')
 model.set_generation_params(duration=30)
 
 @spaces.GPU(duration=120)
 def generate_music(description, melody_audio):
     with autocast():
-        logging.info("Iniciando a geração de música.")
+        logging.info("Starting the music generation.")
         if description:
             description = [description]
             if melody_audio:
-                logging.info(f"Carregando a melodia de áudio de: {melody_audio}")
+                logging.info(f"Loading the audio melody of: {melody_audio}")
                 melody, sr = torchaudio.load(melody_audio)
-                logging.info("Gerando música com descrição e melodia.")
+                logging.info("Generating music with description and melody.")
                 wav = model.generate_with_chroma(description, melody[None], sr)
             else:
-                logging.info("Gerando música apenas com descrição.")
+                logging.info("Generating music solely from description.")
                 wav = model.generate(description)
         else:
-            logging.info("Gerando música de forma incondicional.")
+            logging.info("Generating music unconditionally.")
             wav = model.generate_unconditional(1)
         filename = f'{str(uuid.uuid4())}'
         logging.info(f"Salvando a música gerada com o nome: {filename}")
         path = audio_write(filename, wav[0].cpu().to(torch.float32), model.sample_rate, strategy="loudness", loudness_compressor=True)
-        print("Música salva em", path, ".")
+        print("Music saved in", path, ".")
         # Verifica a forma do tensor de áudio e se foi salvo corretamente
-        logging.info(f"A forma do tensor de áudio gerado: {wav[0].shape}")
-        logging.info("Música gerada e salva com sucesso.")
+        logging.info(f"The shape of the generated audio tensor: {wav[0].shape}")
+        logging.info("Music generated and saved successfully.")
         if not os.path.exists(path):
             raise ValueError(f'Failed to save audio to {path}')
 
@@ -71,6 +71,7 @@ gr.Interface(
                 "Space created by [artificialguybr](https://twitter.com/artificialguybr) on Twitter.",
     examples=[
         ["trap, synthesizer, songstarters, dark, G# minor, 140 bpm", "./assets/kalhonaho.mp3"],
+        ["sega genesis, 8bit, dark, 140 bpm", None],
         ["upbeat, electronic, synth, dance, 120 bpm", None]
     ]
 ).launch()
